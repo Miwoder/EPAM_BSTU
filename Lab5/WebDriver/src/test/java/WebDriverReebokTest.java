69aad8de622733f937aecb069832f130f8ff86b7
Miwoder <bestboy75@rambler.ru>
2020-12-02 01:27:03 +0300

    delete second test

--- Lab5/WebDriver/src/test/java/WebDriverReebokTest.java
+++ Lab5/WebDriver/src/test/java/WebDriverReebokTest.java
@@ -18,26 +18,11 @@ public class WebDriverReebokTest {
     }
 
     @Test
-    public void addToWishListNewManBootsTest() {
-        driver.get("https://www.reebok.ru/krossovki-reebok-classic-leather/FZ1188.html");
-        WebElement addToWishListButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div[2]/div[2]/section/div[3]/div/div"));
-        addToWishListButton.click();
-        WebElement goToWishListButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[1]/div/div[3]/div/div[2]/div[3]"));
-        goToWishListButton.click();
-        new WebDriverWait(driver, 3)
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[3]/div[1]/a/span")));
-        WebElement closeDialog = driver.findElement(By.xpath("/html/body/div[3]/div[1]/a/span"));
-        closeDialog.click();
-        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"dwfrm_wishlist_items\"]/div[1]/div/a")).getText(),
-                "Кроссовки Reebok Classic Leather");
-    }
-
-    @Test
     public void addToWishListNewManBootsEngTest() {
         driver.get("https://www.reebok.com/us/reebok-nano-x-shoes/FV6672.html");
-        WebElement addToWishListButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div[2]/div[2]/section/div[3]/div/div"));
+        WebElement addToWishListButton = driver.findElement(By.xpath("//div[@data-auto-id=\"wishlist-button\"]"));
         addToWishListButton.click();
-        WebElement goToWishListButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[1]/div/div[3]/div/div[2]/div[3]"));
+        WebElement goToWishListButton = driver.findElement(By.xpath("//a[@data-auto-id=\"cart-wishlist-icon-header\"]"));
         goToWishListButton.click();
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"dwfrm_wishlist_items\"]/div[1]/div/a")).getText(),
                         "Reebok Nano X Shoes");
