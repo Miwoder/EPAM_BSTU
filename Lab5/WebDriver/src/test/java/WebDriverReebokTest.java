229f016f7d6aeb13f266f7e240edb0011bd730d5
Miwoder <bestboy75@rambler.ru>
2020-12-02 01:31:14 +0300

    add service

--- Lab5/WebDriver/src/test/java/WebDriverReebokTest.java
+++ Lab5/WebDriver/src/test/java/WebDriverReebokTest.java
@@ -2,18 +2,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class WebDriverReebokTest {
     private WebDriver driver;
+    private static ChromeDriverService service;
 
     @BeforeMethod(alwaysRun = true)
-    public void driverSetup(){
+    public void driverSetup() throws IOException {
+        System.setProperty("webdriver.chrome.driver","C:\\TAT_EPAM_LABS\\ChromeDriver\\chromedriver.exe");
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("C:\\TAT_EPAM_LABS\\ChromeDriver\\chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        service.start();
         driver = new ChromeDriver();
     }
 
