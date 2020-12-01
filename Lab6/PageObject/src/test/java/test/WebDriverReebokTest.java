e6b8cb72978b4d4590a864b05b77feb376ba18c6
Miwoder <bestboy75@rambler.ru>
2020-12-02 01:05:12 +0300

    before

--- Lab6/PageObject/src/test/java/test/WebDriverReebokTest.java
+++ Lab6/PageObject/src/test/java/test/WebDriverReebokTest.java
@@ -2,20 +2,30 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.ReebokSneakerPage;
 
+import java.io.File;
+import java.io.IOException;
+
 public class WebDriverReebokTest {
     private WebDriver driver;
+    private static ChromeDriverService service;
     private final String expectedWishListResult = "Zig Kinetica Shoes";
     private final String expectedCartResult =  "ZIG KINETICA SHOES";
 
     @BeforeMethod(alwaysRun = true)
-    public void driverSetup(){
+    public void driverSetup() throws IOException {
         System.setProperty("webdriver.chrome.driver","C:\\TAT_EPAM_LABS\\ChromeDriver\\chromedriver.exe");
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("C:\\TAT_EPAM_LABS\\ChromeDriver\\chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        service.start();
         driver = new ChromeDriver();
     }
 
